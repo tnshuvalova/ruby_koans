6f28a37c468e338b4c19f7cb516e056c881086e8
tnshuvalova <tshuvalova@cogniance.com>
2014-10-01 19:47:37 +0300

    task

--- src/about_asserts.rb
+++ src/about_asserts.rb
@@ -6,17 +6,7 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 class AboutAsserts < EdgeCase::Koan
 
   # We shall contemplate truth by testing reality, via asserts.
-  def test_assert_truth
-    #--
-    assert true                 # This should be true
-    if false
-    #++
-    assert false                # This should be true
-    #--
-    end
-    #++
-  end
-
+ 
   # Enlightenment may be more easily achieved with appropriate
   # messages.
   def test_assert_with_message
